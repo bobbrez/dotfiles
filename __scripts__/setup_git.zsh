dd3f56974de1ced0fa5345e43a53829427b5e09e
Bob Breznak <143331+bobbrez@users.noreply.github.com>
2021-11-23 11:57:47 -0500

    Update dotfiles to use ssh path

--- __scripts__/setup_git.zsh
+++ __scripts__/setup_git.zsh
@@ -1,5 +1,11 @@
 [ -f ~/.gitignore ] && echo "Backing up existing .gitignore" && cp ~/.gitignore ~/.gitignore.bkup && rm ~/.gitignore
-ln -s ~/.dotfiles/terminal/gitignore ~/.gitignore
+ln -s ~/.dotfiles/git/gitignore ~/.gitignore
 
 [ -f ~/.gitconfig ] && echo "Backing up existing .gitconfig"  && cp ~/.gitconfig ~/.gitconfig.bkup && rm ~/.gitconfig
-ln -s ~/.dotfiles/terminal/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+
+cd ~/.dotfiles
+git remote rm origin
+git remote add origin git@github.com:bobbrez/dotfiles.git
+
+cd ~
