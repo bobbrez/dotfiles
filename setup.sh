b35d81016f1d8a58c7192a068baaf4502b0b3789
Bob Breznak <143331+bobbrez@users.noreply.github.com>
2022-04-24 22:18:48 -0400

    Reorder to setup Homebrew first

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 echo "Setting up OSX"
 ./.dotfiles/__scripts__/setup_osx.sh
 
-echo "Setting up Apps"
-./.dotfiles/__scripts__/setup_apps.zsh
-
 echo "Setting up Homebrew"
 ./.dotfiles/__scripts__/setup_homebrew.zsh
+
+echo "Setting up Apps"
+./.dotfiles/__scripts__/setup_apps.zsh
 
 echo "Setting up iTerm2"
 ./.dotfiles/__scripts__/setup_iterm2.zsh
