2aabb4b5b5335425f2ab96791dff728505291854
Bob Breznak <143331+bobbrez@users.noreply.github.com>
2022-04-24 22:22:04 -0400

    Update Homebrew install script

--- __scripts__/setup_homebrew.zsh
+++ __scripts__/setup_homebrew.zsh
@@ -1,5 +1,5 @@
 if ! command -v brew info &> /dev/null
-    brew update
-then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+then
+    brew update
 fi
